9e1d33633835b242f9a9bccbdb527f0188fcf5a4
4scencio <alessandroascencio@protonmail.com>
2021-01-27 03:04:43 -0800

    script frontend

--- public/script.js
+++ public/script.js
@@ -4,30 +4,57 @@ const socket = io({
     transports: ["websocket"]
 });
 
+        const nickname = document.querySelector('#nickname').value
+
+        const sendInput = document.querySelector('#sendInput')
+
+        const sendMessage = () => {
+            const message = sendInput.value.trim()
+
+            if(!message) return
+            sendInput.value = ''
+
+            socket.emit('message', { author: nickname, message: message })
+        }
+
+            socket.on('msg', (data) => {
+                io.emit('showMsg', data)
+            })
+
+        socket.on('connect', () => {
+            console.log(socket.id)
+            const chat = document.querySelector('#chat') 
+            const message = document.createElement('p')
+            const hours = moment().format('hh:mm');
+        
+            message.innerHTML = `<strong> <center> - Bem-vindo ao chat</strong> MSX -> 👌`
+        
+            chat.append(message)
+            chat.scrollTop = chat.scrollHeight
+        })
+
         socket.on('disconnect',() => {
             console.log('Desconectado')
         })
 
-        socket.on('showMsg', data => {
-            const divChat = document.getElementById('chat')
-            const p = document.createElement('p')
-            console.log(data)
-            p.innerHTML = `<strong>${data.username}:</strong> <i>(${data.hours})</i>: ${data.msg}`
-            divChat.append(p)
+        socket.on('renderMessage', (data) => {
+            const chat = document.querySelector('#chat') 
+            const message = document.createElement('p')
+            let hours = moment().format('hh:mm');
+        
+            message.innerHTML = `<i>(${hours})</i> ${data.author}: ${data.message}`
+        
+            chat.append(message)
+            chat.scrollTop = chat.scrollHeight
         })
+        
+            socket.on('newPerson', count => {
+                const online = document.getElementById('online')
+                online.innerHTML = `Usuários <style="color: green">online</style>: ${count}`
 
-        var usernameField = prompt('Digite seu nome')
 
-        function enviarMsg() {
-            let msgField = document.getElementById('msg')
-            // let usernameField = window.prompt('Digite seu nome')
-            moment.locale('pt-br')
-            let hours = moment().format('hh:mm');    
-            let msg = msgField.value
-            let username = usernameField
-
-            socket.emit('msg', {msg: msg, username: username, hours: hours})
-        }
-
-        let btn = document.getElementById('btn')
-        btn.addEventListener('click', enviarMsg)
+        //Enviar Mensagens        
+        sendInput.addEventListener('keyup',e => {
+            if(e.key === 'Enter') sendMessage()
+        })
+    })
